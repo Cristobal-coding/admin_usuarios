8267f7bd44409b1118cac248bb439fc72800dd0c
Cristobal-code <cristobalh517@gmail.com>
2023-09-01 16:49:58 -0400

    implements service impl

--- src/main/java/adminUsuarios/usm/cl/services/UsuarioServiceImpl.java
+++ src/main/java/adminUsuarios/usm/cl/services/UsuarioServiceImpl.java
@@ -2,21 +2,28 @@ package adminUsuarios.usm.cl.services;
 
 import adminUsuarios.usm.cl.entities.Usuario;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UsuarioServiceImpl implements  UsuarioService{
+
+    public static List<Usuario> usuarios= new ArrayList<>();
     @Override
     public List<Usuario> getAll() {
-        return null;
+        return usuarios;
     }
 
     @Override
     public Usuario createUsuario(Usuario usuario) {
-        return null;
+
+        usuarios.add(usuario);
+        return usuario;
     }
 
     @Override
     public Usuario findUsuario(String email) {
-        return null;
+
+
+        return usuarios.stream().filter(usuario -> usuario.getEmail().equalsIgnoreCase(email)).findFirst().orElse(null);
     }
 }
