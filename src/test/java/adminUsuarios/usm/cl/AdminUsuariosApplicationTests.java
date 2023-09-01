eedfd58dc5d54bc91945625214d865b93eab0245
Cristobal-code <cristobalh517@gmail.com>
2023-09-01 16:03:50 -0400

    first commit

---
+++ src/test/java/adminUsuarios/usm/cl/AdminUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package adminUsuarios.usm.cl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
